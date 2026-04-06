4b5b4e1350d6c319c4deefc1b175d6db705c70ae
stepaniukserhii-create <stepaniuk.serhii@e-u.edu.ua>
2026-04-06 15:42:09 +0300

    Update task04.rs

--- src/hackerrank/task04.rs
+++ src/hackerrank/task04.rs
@@ -1,1 +1,28 @@
 // https://www.hackerrank.com/challenges/grading/problem
+using System;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            int grade = int.Parse(Console.ReadLine());
+
+            if (grade >= 38)
+            {
+                int nextMultiple = ((grade / 5) + 1) * 5;
+
+                if (nextMultiple - grade < 3)
+                {
+                    grade = nextMultiple;
+                }
+            }
+
+            Console.WriteLine(grade);
+        }
+    }
+}
+
