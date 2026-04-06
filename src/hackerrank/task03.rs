ad2084e9199d0851cd9806e51e7575f274dd6675
stepaniukserhii-create <stepaniuk.serhii@e-u.edu.ua>
2026-04-06 15:40:51 +0300

    Update task03.rs

--- src/hackerrank/task03.rs
+++ src/hackerrank/task03.rs
@@ -1,1 +1,19 @@
 // https://www.hackerrank.com/challenges/staircase/problem
+using System;
+
+class Solution
+{
+    static void staircase(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(new string(' ', n - i) + new string('#', i));
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        staircase(n);
+    }
+}
