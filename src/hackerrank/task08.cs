69c2eb6bd9d6eb1b0baee601e5e5c2daa06e99de
stepaniukserhii-create <stepaniuk.serhii@e-u.edu.ua>
2026-04-20 11:19:11 +0300

    Update task08.cs

--- src/hackerrank/task08.cs
+++ src/hackerrank/task08.cs
@@ -1,1 +1,34 @@
 // https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] scores = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int maxScore = scores[0];
+        int minScore = scores[0];
+
+        int maxBreaks = 0;
+        int minBreaks = 0;
+
+        for (int i = 1; i < n; i++)
+        {
+            if (scores[i] > maxScore)
+            {
+                maxScore = scores[i];
+                maxBreaks++;
+            }
+            else if (scores[i] < minScore)
+            {
+                minScore = scores[i];
+                minBreaks++;
+            }
+        }
+
+        Console.WriteLine($"{maxBreaks} {minBreaks}");
+    }
+}
