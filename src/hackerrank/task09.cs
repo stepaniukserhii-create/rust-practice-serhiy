b8a07fe37b8357aeff389b674098f0f92bf8f849
stepaniukserhii-create <stepaniuk.serhii@e-u.edu.ua>
2026-05-01 10:05:29 +0300

    Update task09.cs

--- src/hackerrank/task09.cs
+++ src/hackerrank/task09.cs
@@ -1,1 +1,37 @@
 // https://www.hackerrank.com/challenges/migratory-birds/problem
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int[] arr = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+        int[] count = new int[6];
+
+        foreach (int bird in arr)
+        {
+            count[bird]++;
+        }
+
+        int maxCount = 0;
+        int result = 1;
+
+        for (int i = 1; i <= 5; i++)
+        {
+            if (count[i] > maxCount)
+            {
+                maxCount = count[i];
+                result = i;
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+}
