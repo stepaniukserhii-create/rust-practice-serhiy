8c434372241d85cb6cd73ab2e95ab4dd41b7c2f4
stepaniukserhii-create <stepaniuk.serhii@e-u.edu.ua>
2026-05-08 10:38:03 +0300

    Update task10.cs

--- src/hackerrank/task10.cs
+++ src/hackerrank/task10.cs
@@ -1,1 +1,32 @@
 // https://www.hackerrank.com/challenges/sock-merchant/problem
+using System;
+using System.Linq;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int[] arr = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+        int[] count = new int[101];
+
+        foreach (int sock in arr)
+        {
+            count[sock]++;
+        }
+
+        int pairs = 0;
+
+        for (int i = 0; i < count.Length; i++)
+        {
+            pairs += count[i] / 2;
+        }
+
+        Console.WriteLine(pairs);
+    }
+}
