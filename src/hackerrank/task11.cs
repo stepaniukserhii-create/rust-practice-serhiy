0d713c6338c8b40d5f2e91bd2e179fdea4d9c55c
stepaniukserhii-create <stepaniuk.serhii@e-u.edu.ua>
2026-05-11 11:48:12 +0300

    Update task11.cs

--- src/hackerrank/task11.cs
+++ src/hackerrank/task11.cs
@@ -1,1 +1,38 @@
 // https://www.hackerrank.com/challenges/diagonal-difference/problem
+using System;
+
+class Solution
+{
+    static int DiagonalDifference(int[][] arr)
+    {
+        int n = arr.Length;
+
+        int primary = 0;
+        int secondary = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            primary += arr[i][i];
+            secondary += arr[i][n - 1 - i];
+        }
+
+        return Math.Abs(primary - secondary);
+    }
+
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int[][] arr = new int[n][];
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            arr[i] = Array.ConvertAll(input, int.Parse);
+        }
+
+        int result = DiagonalDifference(arr);
+
+        Console.WriteLine(result);
+    }
+}
