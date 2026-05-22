189e7744af2d8b75b5ba47b6c2c94b2e2f252727
stepaniukserhii-create <stepaniuk.serhii@e-u.edu.ua>
2026-05-22 14:07:07 +0300

    Update task12.cs

--- src/hackerrank/task12.cs
+++ src/hackerrank/task12.cs
@@ -1,1 +1,16 @@
+https://www.hackerrank.com/challenges/drawing-book/problem
+using System;
 
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int p = int.Parse(Console.ReadLine());
+
+        int fromFront = p / 2;
+        int fromBack = n / 2 - p / 2;
+
+        Console.WriteLine(Math.Min(fromFront, fromBack));
+    }
+}
